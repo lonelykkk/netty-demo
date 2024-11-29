03f7ec53ec803fb744d10be33f1a5c481e2a4e91
lonelykkk <2765314967@qq.com>
2024-11-29 15:34:04 +0800

    p38-nio-selector-处理可写事件

--- src/test/java/cn/itcast/netty/c4/WriteServer.java
+++ src/test/java/cn/itcast/netty/c4/WriteServer.java
@@ -36,19 +36,34 @@ public class WriteServer {
                 if (key.isAcceptable()) {
                     SocketChannel sc = ssc.accept();
                     sc.configureBlocking(false);
-                    //sc.register(selector, SelectionKey.OP_WRITE);
+                    SelectionKey scKey = sc.register(selector, 0, null);
                     // 1. 向客户端发送大量数据
                     StringBuilder sb = new StringBuilder();
                     for (int i = 0; i < 3000000; i++) {
                         sb.append("a");
                     }
                     ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
+                    // 2. 返回值代表实际写入的字节数
+                    int write = sc.write(buffer);
+                    System.out.println(write);
+                    // 3. 判断是否有剩余内容
+                    if (buffer.hasRemaining()) {
+                        // 4. 关注可写事件
+                        scKey.interestOps(SelectionKey.OP_WRITE);
+                        // 5. 把未写完的数据挂到 scKey 上
+                        scKey.attach(buffer);
+                    }
+                } else if (key.isWritable()) {
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    SocketChannel sc = (SocketChannel) key.channel();
 
-                    while (buffer.hasRemaining()) {
-                        // 2. 返回值代表实际写入的字节数
-                        int write = sc.write(buffer);
-                        System.out.println(write);
+                    int write = sc.write(buffer);
+                    System.out.println(write);
 
+                    // 6. 清理操作
+                    if (!buffer.hasRemaining()) {
+                        key.attach(null);
+                        key.interestOps(key.interestOps() - SelectionKey.OP_WRITE); // 写完了就不需要再关注可写事件
                     }
                 }
             }
