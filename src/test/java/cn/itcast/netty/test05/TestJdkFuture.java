aaa5368dec42b534f98c68299dea633e6b04d3a2
lonelykkk <2765314967@qq.com>
2024-12-05 16:19:51 +0800

    P72-netty入门-jdk-future

---
+++ src/test/java/cn/itcast/netty/test05/TestJdkFuture.java
@@ -0,0 +1,32 @@
+package cn.itcast.netty.test05;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+/**
+ * @author lonelykkk
+ * @email 2765314967@qq.com
+ * @date 2024/12/5 15:55
+ * @Version V1.0
+ */
+@Slf4j
+public class TestJdkFuture {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // 1. 线程池
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        // 2. 提交任务
+        Future<Integer> future = service.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                log.debug("执行计算");
+                TimeUnit.SECONDS.sleep(1);
+                return 50;
+            }
+        });
+
+        // 3. 主线程通过 future 获取结果
+        log.debug("等待结果");
+        log.debug("结果是：{}", future.get());
+    }
+}
