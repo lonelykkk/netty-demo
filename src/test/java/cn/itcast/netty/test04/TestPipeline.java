afec3a4cdfef3a3271437eadb27fb58a408833f8
lonelykkk <2765314967@qq.com>
2024-12-05 17:30:22 +0800

    P76-netty入门-inbound-handler

--- src/test/java/cn/itcast/netty/test04/TestPipeline.java
+++ src/test/java/cn/itcast/netty/test04/TestPipeline.java
@@ -1,13 +1,18 @@
 package cn.itcast.netty.test04;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.nio.charset.Charset;
+
 /**
+ * P76-netty入门-inbound-handler 入栈处理器
  * @author lonelykkk
  * @email 2765314967@qq.com
  * @date 2024/12/5 16:51
@@ -31,22 +36,25 @@ public class TestPipeline {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                 log.debug("1");
-
-                                super.channelRead(ctx, msg);
+                                ByteBuf buf = (ByteBuf) msg;
+                                String name = buf.toString(Charset.defaultCharset());
+                                super.channelRead(ctx, name);
                             }
                         });
                         pipeline.addLast("h2",new ChannelInboundHandlerAdapter(){
                             @Override
-                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            public void channelRead(ChannelHandlerContext ctx, Object name) throws Exception {
                                 log.debug("2");
-
-                                super.channelRead(ctx, msg);
+                                Student student = new Student((String) name);
+                                // 2选一
+                                super.channelRead(ctx, student); //将数据传给下一个handler，不调用相当于断开了
+                                // ctx.fireChannelRead(student);
                             }
                         });
                         pipeline.addLast("h3",new ChannelInboundHandlerAdapter(){
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                log.debug("3");
+                                log.debug("3,结果：{},class:{}", msg,msg.getClass());
                                 super.channelRead(ctx, msg);
                                 ch.writeAndFlush(ctx.alloc().buffer().writeBytes("server".getBytes()));
                             }
@@ -77,5 +85,14 @@ public class TestPipeline {
                     }
                 })
                 .bind(8080);
+    }
+
+    @Data
+    static class Student {
+        private String name;
+
+        public Student(String name) {
+            this.name = name;
+        }
     }
 }
