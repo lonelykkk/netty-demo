5d8c19cbcf0391c733f1e06a2b77bbaa7cb86f03
lonelykkk <2765314967@qq.com>
2024-12-05 09:32:00 +0800

    P62-netty入门-eventLoop-分工细化

---
+++ src/test/java/cn/itcast/netty/test02/EventLoopClient2.java
@@ -0,0 +1,32 @@
+package cn.itcast.netty.test02;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+
+@Slf4j
+public class EventLoopClient2 {
+    public static void main(String[] args) throws InterruptedException {
+        Channel channel = new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync()
+                .channel();
+        System.out.println(channel);
+        System.out.println("");
+    }
+}
---
+++ src/test/java/cn/itcast/netty/test02/EventLoopServer2.java
@@ -0,0 +1,51 @@
+package cn.itcast.netty.test02;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+/** eventLoop 分工细化
+ * @author lonelykkk
+ * @email 2765314967@qq.com
+ * @date 2024/12/4 18:43
+ * @Version V1.0
+ */
+@Slf4j
+public class EventLoopServer2 {
+    public static void main(String[] args) {
+        // 细分2 创建一个独立的 EventLoopGroup
+        EventLoopGroup group = new DefaultEventLoopGroup();
+        new ServerBootstrap()
+                // boss 和 worker
+                // boss 只负责accept worker只负责SocketChannel上的读写
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    //initChannel 方法会在连接建立后被调用
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast("handler1",new ChannelInboundHandlerAdapter() {
+                            @Override                                         //msg: ByteBuf
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                                ctx.fireChannelRead(msg);  // 将消息传递给下一个handler
+                            }
+                        }).addLast(group,"handler2",new ChannelInboundHandlerAdapter() {
+                            @Override                                         //msg: ByteBuf
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+}
