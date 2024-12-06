b7649746b2c87fd3611ff1227ba7e53461cd3882
lonelykkk <2765314967@qq.com>
2024-12-06 16:47:14 +0800

    P87-netty入门-byteBuf-零拷贝-composite

---
+++ src/test/java/cn/itcast/netty/test05/TestCompositeByteBuf.java
@@ -0,0 +1,28 @@
+package cn.itcast.netty.test05;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
+
+import static cn.itcast.netty.test04.TestByteBuf.log;
+
+
+public class TestCompositeByteBuf {
+    public static void main(String[] args) {
+        ByteBuf buf1 = ByteBufAllocator.DEFAULT.buffer();
+        buf1.writeBytes(new byte[]{1, 2, 3, 4, 5});
+
+        ByteBuf buf2 = ByteBufAllocator.DEFAULT.buffer();
+        buf2.writeBytes(new byte[]{6, 7, 8, 9, 10});
+
+        /*ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+        //这样会进行两次数据复制，如果数据量大的情况下很不友好，不推荐
+        buffer.writeBytes(buf1).writeBytes(buf2);
+        log(buffer);*/
+
+        CompositeByteBuf buffer = ByteBufAllocator.DEFAULT.compositeBuffer();
+        buffer.addComponents(true, buf1, buf2);
+        log(buffer);
+
+    }
+}
