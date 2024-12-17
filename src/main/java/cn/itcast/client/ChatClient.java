3839ffcd196db87eaebd72e9ffa822b22b7346f5
lonelykkk <2765314967@qq.com>
2024-12-17 18:55:13 +0800

    p112-netty进阶-聊天业务-业务消息发送

--- src/main/java/cn/itcast/client/ChatClient.java
+++ src/main/java/cn/itcast/client/ChatClient.java
@@ -1,6 +1,6 @@
 package cn.itcast.client;
 
-import cn.itcast.message.LoginRequestMessage;
+import cn.itcast.message.*;
 import cn.itcast.protocol.MessageCodecSharable;
 import cn.itcast.protocol.ProcotolFrameDecoder;
 import io.netty.bootstrap.Bootstrap;
@@ -15,7 +15,12 @@ import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 public class ChatClient {
@@ -23,6 +28,8 @@ public class ChatClient {
         NioEventLoopGroup group = new NioEventLoopGroup();
         LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
         MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        CountDownLatch WAIT_FOR_LOGIN = new CountDownLatch(1);
+        AtomicBoolean LOGIN = new AtomicBoolean(false);
         try {
             Bootstrap bootstrap = new Bootstrap();
             bootstrap.channel(NioSocketChannel.class);
@@ -31,16 +38,24 @@ public class ChatClient {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
                     ch.pipeline().addLast(new ProcotolFrameDecoder());
-                    ch.pipeline().addLast(LOGGING_HANDLER);
+                //    ch.pipeline().addLast(LOGGING_HANDLER);
                     ch.pipeline().addLast(MESSAGE_CODEC);
                     ch.pipeline().addLast("client handler", new ChannelInboundHandlerAdapter() {
 
                         // 接收响应消息
-
-
                         @Override
                         public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                             log.debug("msg: {}",msg);
+                            if (msg instanceof LoginResponseMessage) {
+                                LoginResponseMessage response = (LoginResponseMessage) msg;
+                                if (response.isSuccess()) {
+                                    // 如果登录成功
+                                    LOGIN.set(true);
+                                }
+                                // 唤醒 System.in 的线程
+                                WAIT_FOR_LOGIN.countDown();
+                            }
+
                         }
 
                         //连接建立事件
@@ -59,6 +74,54 @@ public class ChatClient {
                                 ctx.writeAndFlush(message);
 
                                 System.out.println("等待后续操作...");
+                                try {
+                                    WAIT_FOR_LOGIN.await();
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                // 如果登录失败
+                                if (!LOGIN.get()) {
+                                    ctx.channel().close();
+                                    return;
+                                }
+                                while (true) {
+                                    System.out.println("==================================");
+                                    System.out.println("send [username] [content]");
+                                    System.out.println("gsend [group name] [content]");
+                                    System.out.println("gcreate [group name] [m1,m2,m3...]");
+                                    System.out.println("gmembers [group name]");
+                                    System.out.println("gjoin [group name]");
+                                    System.out.println("gquit [group name]");
+                                    System.out.println("quit");
+                                    System.out.println("==================================");
+                                    String command = scanner.nextLine();
+                                    String[] s = command.split(" ");
+                                    switch (s[0]){
+                                        case "send":
+                                            ctx.writeAndFlush(new ChatRequestMessage(username, s[1], s[2]));
+                                            break;
+                                        case "gsend":
+                                            ctx.writeAndFlush(new GroupChatRequestMessage(username, s[1], s[2]));
+                                            break;
+                                        case "gcreate":
+                                            Set<String> set = new HashSet<>(Arrays.asList(s[2].split(",")));
+                                            set.add(username); // 加入自己
+                                            ctx.writeAndFlush(new GroupCreateRequestMessage(s[1], set));
+                                            break;
+                                        case "gmembers":
+                                            ctx.writeAndFlush(new GroupMembersRequestMessage(s[1]));
+                                            break;
+                                        case "gjoin":
+                                            ctx.writeAndFlush(new GroupJoinRequestMessage(username, s[1]));
+                                            break;
+                                        case "gquit":
+                                            ctx.writeAndFlush(new GroupQuitRequestMessage(username, s[1]));
+                                            break;
+                                        case "quit":
+                                            ctx.channel().close();
+                                            return;
+                                    }
+                                }
                             },"system in").start();
                         }
                     });
