3dee9db49216dd2a59771bb846a07d6ef029ed61
lonelykkk <2765314967@qq.com>
2024-12-22 17:49:58 +0800

    p116-netty进阶-聊天业务-空闲检测

--- src/main/java/cn/itcast/server/ChatServer.java
+++ src/main/java/cn/itcast/server/ChatServer.java
@@ -8,16 +8,16 @@ import cn.itcast.server.handler.*;
 import cn.itcast.server.service.UserServiceFactory;
 import cn.itcast.server.session.SessionFactory;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -42,6 +42,22 @@ public class ChatServer {
             serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
+                    // new IdleStateHandler() 用来判断是不是 读写空闲时间过长
+                    // new IdleStateHandler(5,0,0) 5s内如果没有收到channel数据，会触发一个事件
+                    ch.pipeline().addLast(new IdleStateHandler(5,0,0));
+                    // ChannelDuplexHandler 可以同时作为入站和出站处理器
+                    ch.pipeline().addLast(new ChannelDuplexHandler() {
+                        // 用来触发特殊事件
+                        @Override
+                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception{
+                            IdleStateEvent event = (IdleStateEvent) evt;
+                            // 触发了读空闲事件
+                            if (event.state() == IdleState.READER_IDLE) {
+                                log.debug("已经 5s 没有读到数据了");
+                                ctx.channel().close();
+                            }
+                        }
+                    });
                     ch.pipeline().addLast(new ProcotolFrameDecoder());
                     ch.pipeline().addLast(LOGGING_HANDLER);
                     ch.pipeline().addLast(MESSAGE_CODEC);
@@ -55,6 +71,7 @@ public class ChatServer {
                     ch.pipeline().addLast(QUIT_HANDLER);
                 }
             });
+
             Channel channel = serverBootstrap.bind(8080).sync().channel();
             channel.closeFuture().sync();
         } catch (InterruptedException e) {
